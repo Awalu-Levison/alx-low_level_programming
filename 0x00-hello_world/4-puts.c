f756fd8be33b41d3b0960efc3dcc7c461388c106
Awalu-Levison <levisonawalu251@gmail.com>
2023-07-14 15:07:56 -0700

    Wtriting simple programs with C programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program execution
+ * Description: A simple program in C to print special lines of text
+ * Return: 0 ALways success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
