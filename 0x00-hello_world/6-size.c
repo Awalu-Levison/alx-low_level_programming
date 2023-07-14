b6f959a763fa40e3abaacdbed36f93e854f3efc1
Awalu-Levison <levisonawalu251@gmail.com>
2023-07-14 16:38:40 -0700

    Generating the size of differemt data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 /**
  * main - Entry point of program execution
- *
  * Return: 0 (Zero on successful compilation)
  */
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long int);
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int);
-	printf("Size of a float: %lu byte(s)0\n", sizeof(float);
+	char j;
+	int age;
+	long int phone;
+	long long int addr;
+	float floating;
+
+	/*Size declaration*/
+	printf("Size of a char: %lu byte(s)\n", sizeof(j));
+	printf("Size of an int: %lu byte(s)\n", sizeof(age));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(phone);
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int addr);
+	printf("Size of a float: %lu byte(s)0\n", sizeof(floating);
+
+	return (0);
 }
