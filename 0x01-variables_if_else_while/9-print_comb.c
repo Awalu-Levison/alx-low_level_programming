29b5f3e7222691d751132b650997aa7286fae081
Awalu-Levison <levisonawalu251@gmail.com>
2023-07-16 07:45:34 -0700

    Printing separated by comma followed by space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program execution
+ * Description - Printing numbers separated by comma
+ * Return: (0 On successful program execution
+ */
+int main(void)
+{
+	int special_numbers;
+
+	for (special_numbers = 0; special_numbers < 10; special_numbers++)
+	{
+		putchar(special_numbers);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+}
