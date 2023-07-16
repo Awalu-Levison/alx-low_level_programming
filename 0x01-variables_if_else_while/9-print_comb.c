827b8e6a1db42405a0587d7fd83e7db180960ce1
Awalu-Levison <levisonawalu251@gmail.com>
2023-07-16 07:57:42 -0700

    Printing separated by comma followed by space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,15 @@ int main(void)
 {
 	int special_numbers;
 
-	for (special_numbers = '0'; special_numbers < '10'; special_numbers++)
+	for (special_numbers = 0; special_numbers < 10; special_numbers++)
 	{
-		putchar(special_numbers);
-		putchar(',');
-		putchar(' ');
+		putchar((special_numbers % 10) + '0');
+		if (special_numbers == 9)
+		{
+			continue;
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
