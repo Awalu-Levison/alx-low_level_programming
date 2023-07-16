071451bef9d79db477c7025059cda02c8c22cd5f
Awalu-Levison <levisonawalu251@gmail.com>
2023-07-15 20:57:21 -0700

    Printing numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program execution
+ * Description: Print numbers of base 10 starting from zero to nine
+ * Return: (Zero on successful compilation)
+ */
+int main(void)
+{
+	int nambaz;
+
+	for (nambaz = 0; nambaz < 10; nambaz++)
+	{
+		printf(nambaz);
+	}
+	printf("\n");
+}
