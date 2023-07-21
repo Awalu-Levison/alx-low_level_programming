01be7ed6a5ce72653f8c774fc35d548b115b8dbd
Awalu-Levison <levisonawalu251@gmail.com>
2023-07-20 19:59:57 -0700

    Lower case or upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - Printing upper cases
+ * @c: Parameter to check at
+ * Return: 0 Always success
+ */
+int _isupper(int c)
+{
+	if ((c > 65) && (c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
