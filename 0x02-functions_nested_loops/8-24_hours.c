8d59bc99fa863d44c9efe1e3f53de54b91adfdeb
Awalu-Levison <levisonawalu251@gmail.com>
2023-07-21 22:09:34 -0700

    Printing time for Jackboer travelling

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,4 +5,13 @@
  */
 void jack_bauer(void)
 {
+	int num1, num2, num3, num4;
+
+	for (num1 = 0; num1 <= 2; num1++)
+	{
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			for (num3 = 0; num3 <= 5; num3++)
+			{
+				for (num4 = 0; num4 <= 9; num4++)
 }
