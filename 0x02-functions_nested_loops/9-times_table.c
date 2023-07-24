f945b83bbb316367d574e55355a08cbad950bdb5
Awalu-Levison <levisonawalu251@gmail.com>
2023-07-24 14:25:32 -0700

    Official 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,31 +5,31 @@
  */
 void times_table(void)
 {
-	int num1, num2, num3, num4, num5;
+	int num1, num2, num3;
 
 	for (num1 = 0; num1 <= 9; num1)
 	{
+		_putchar('0');
 		for (num2 = 0; num2 <= 9; num2++)
 		{
 			num3 = num1 * num2;
-			if (num3 > 9)
+			if (num3 == 0)
 			{
-				num4 = num3 % 10;
-				num5 = (num3 - num4) / 10;
-				_putchar(44);
-				_putchar(32);
-				_putchar(num5 + '0');
-				_putchar(num4 + '0');
-			}
-			else
-			{
-				if (num2 != 0)
-				{
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
-				}
 				_putchar(num3 + '0');
+			}
+			if (num3 < 10 && num2 != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(num3 + '0');
+			}
+			else if (num3 >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((num3 / 10) + '0');
+				_putchar((num3 % 10) + '0');
 			}
 		}
 		_putchar('\n');
