21fa1060aa12be655c41d57203d805b73df4448b
Awalu-Levison <levisonawalu251@gmail.com>
2023-07-23 21:24:23 -0700

    A function that add two intergers input by users

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Adds two intergers
+ * Return: 0 On successful exexcution
+ * @m:parameter to add
+ * @v:parameter to add with
+ */
+int add(int m, int v)
+{
+	int sum;
+
+	sum = m + v;
+	return (sum);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,5 +9,5 @@ int _abs(int n);
 int print_last_digit(int i);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int m, int v);
 void print_to_98(int n);
