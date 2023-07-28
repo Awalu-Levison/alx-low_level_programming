53dd8090a1677b7f6af3be0a1b07604960e93259
Awalu-Levison <levisonawalu251@gmail.com>
2023-07-28 16:21:43 -0700

    Printing the square of the number

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_square - Prints the square of given input
+ * @size: parameter to check
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int number;
+
+		for (number = 1; number < size; size++)
+		{
+			_putchar('#');
+		}
+	}
+}
