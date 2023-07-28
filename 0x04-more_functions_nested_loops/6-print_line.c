1fefd3bde2164a641b4c67b7ff7611a17d693dea
Awalu-Levison <levisonawalu251@gmail.com>
2023-07-28 04:02:57 -0700

    A function that draws a straight line in the terminal.

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - A function that prints a line to the terminal
+ * @n:Parameter to check
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		_putchar(n);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
