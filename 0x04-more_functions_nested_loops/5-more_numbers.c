545e39c2c64d6b097e21aaaa0abdab651c5c3ab9
Awalu-Levison <levisonawalu251@gmail.com>
2023-07-27 22:13:43 -0700

    Printing numbers 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  more_numbers - A function that prints number 0 through 14 in 10 times
+ */
+void more_numbers(void)
+{
+	int first_number;
+	char second_number;
+
+	for (first_number = 1; first_number <= 10; first_number++)
+	{
+		for (second_number = '0'; second_number <= '14'; second_number++)
+		{
+			_putchar(second_number);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
