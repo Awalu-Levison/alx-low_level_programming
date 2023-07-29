ce62ed6874558048810e2e8b92cc103b71ce23eb
Awalu-Levison <levisonawalu251@gmail.com>
2023-07-29 11:40:51 -0700

    Printing Fizz Buzz in a series of a number

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,19 +6,19 @@
  */
 int main(void)
 {
-	int number_series;
+	int n;
 
-	for (number_series = 1; number_series <= 100; number_series++)
+	for (n = 1; n <= 100; n++)
 	{
-		if (number_series % 3 == 0)
-			printf("Fizz");
-		else if (number_series % 5 == 0)
-			printf("Buzz");
-		else if (number_series % 15 == 0)
+		if (n % 15 == 0)
 			printf("FizzBuzz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else if (n % 3 == 0)
+			printf("Fizz");
 		else
-			printf("%d", number_series);
-		if (number_series < 100)
+			printf("%d", n);
+		if (n < 100)
 			printf(" ");
 	}
 	printf("\n");
