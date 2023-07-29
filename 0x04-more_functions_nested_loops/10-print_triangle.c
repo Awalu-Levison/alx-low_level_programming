9a8dd950b507549dfb5fe49075b39900494dfbb4
Awalu-Levison <levisonawalu251@gmail.com>
2023-07-29 12:16:54 -0700

    Write a function that prints a triangle, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_triangle - Prints tringle based on the given size
+ * @size:Parameter to check
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar(size);
+			}
+			_putchar('\n');
+		}
+	}
+}
