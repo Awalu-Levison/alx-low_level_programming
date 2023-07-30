de9114b8731757f670c4ea3c72dc50608a89e64b
Awalu-Levison <levisonawalu251@gmail.com>
2023-07-30 10:18:18 -0700

    A function that prints an integer without using long data-type, arrays, pointers and not allowed to hard-code special values

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number - A function that prints intergers only
+ * @n: Parameter to be used
+ */
+void print_number(int n)
+{
+	unsigned int first_value = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		first_value = -first_value;
+	}
+	if (first_value / 10)
+	{
+		print_number(first_value / 10);
+	}
+	_putchar(first_number % 10 + '0');
+}
