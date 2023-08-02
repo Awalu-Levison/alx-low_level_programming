5a74e3f19ed7eb744506dc36c662e7a075ee3853
Awalu-Levison <levisonawalu251@gmail.com>
2023-08-02 06:50:26 -0700

    Advanced n-times table task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,12 +17,13 @@ void print_times_table(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = i; j < n; j++)
+			_putchar('0)'
+			for (j = 0; j < n; j++)
 			{
-				_putchar('0');
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
+
 				if (x <= 9)
 				_putchar(' ');
 			}
