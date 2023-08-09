b225228dc06ed3150932dbf5c08689f78c8e4e3d
Awalu-Levison <levisonawalu251@gmail.com>
2023-08-09 02:00:10 -0700

    Calculating power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,9 +9,9 @@
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-	{
 		return (-1);
-	}
-	x = x ^ y;
-	return (_pow_recursion(x, y));
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
 }
