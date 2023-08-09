dee87e0b777b7c1112b74710900bb4e5521a5fe2
Awalu-Levison <levisonawalu251@gmail.com>
2023-08-09 00:55:08 -0700

    Calculating power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _pow_recursion - Calculating the power of
+ * given number provided with its power
+ * @x: Base number to be used
+ * @y: Power of the base to be used
+ * Return: (0 denotes successful execution)
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	x = x * y;
+	return (x * _pow_recursion(x, y));
+}
