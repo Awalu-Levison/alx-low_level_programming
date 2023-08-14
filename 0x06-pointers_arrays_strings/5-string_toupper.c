e7c9c8b5a6753b90e2bd847c0688b1d0ee473b88
Awalu-Levison <levisonawalu251@gmail.com>
2023-08-14 14:29:15 -0700

    Converting strings to upper cases

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,11 +8,12 @@ char *string_toupper(char *c)
 {
 	int y;
 
-	for (y = 0; c[y] != '\0'; y++)
+	y = 0;
+	while (c[y] != '\0'; y++)
 	{
 		if (c[y] >= 'a' && c[y] <= 'z')
 		{
-			c[y] = c[y] - 32;
+			c[y] -= 32;
 		}
 	}
 }
