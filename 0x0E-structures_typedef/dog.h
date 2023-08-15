54bdfa5133f176e090c5b57f6563edcfaf30d9e6
Awalu-Levison <levisonawalu251@gmail.com>
2023-08-14 20:09:46 -0700

    Understanding struct data type in C

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Basic info of dog
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
