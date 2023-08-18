4fcd7f386cbc64c1107ada2d34954cdc33639152
Awalu-Levison <levisonawalu251@gmail.com>
2023-08-17 18:35:07 -0700

    Encoding strings in C

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,8 +1,24 @@
 #include "main.h"
 /**
- * *leet Encoding strings into number
+ * *leet - Encoding strings into number
  * @n: Pointerto string variable
  * Return: (A character variable)
  */
 char *leet(char *n)
 {
+	int x, y;
+	char first[] = "aAeEoOtTlL";
+	char second[] = "4433007711"
+
+		for (x = 0; n[x] != '\0'; x++)
+		{
+			for (y = 0; first[y] != '\0'; y++)
+			{
+				if (n[x] == first[y])
+				{
+					n[x] = second[y];
+				}
+			}
+		}
+	return (n);
+}
