fb778adeea79f44c8ee4129eb8271c2ee1a47bcb
Awalu-Levison <levisonawalu251@gmail.com>
2023-08-23 05:29:35 -0700

    Adding infinitives

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,17 +1,14 @@
 #include "main.h"
-
 /**
- * rev_string - Experience the art of
- * reversing array
- * @n: integer parameter to be checked upon
- * Return: (0 upon successful execution)
+ * rev_string - The art of reversing arrays
+ * @n: integer parameter variable
+ * Return: 0 upon successful execution
  */
-
 void rev_string(char *n)
 {
 	int i = 0;
 	int j = 0;
-	char m;
+	char temp;
 
 	while (*(n + i) != '\0')
 	{
@@ -21,55 +18,54 @@ void rev_string(char *n)
 
 	for (j = 0; j < i; j++, i--)
 	{
-		m = *(n + j);
+		temp = *(n + j);
 		*(n + j) = *(n + i);
-		*(n + i) = m;
+		*(n + i) = temp;
 	}
 }
 
 /**
- * infinite_add - Adding infinite numbers in C
+ * infinite_add - Adding complex numbers
  * @n1: 1st number to add
  * @n2: 2nd number to add
- * @r: Buffert memory to use
- * @size_r: buffer size
- * Return: pointer to calling function
+ * @r: pointer to buffer
+ * @size_r: Size of the buffer
+ * Return: The pointer to be  called
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int a = 0, b = 0, j = 0, digits = 0;
+	int y = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	while (*(n1 + b) != '\0')
+	while (*(n1 + i) != '\0')
 		i++;
 	while (*(n2 + j) != '\0')
 		j++;
-	b--;
+	i--;
 	j--;
-	if (j >= size_r || b >= size_r)
+	if (j >= size_r || i >= size_r)
 		return (0);
-	while (j >= 0 || b >= 0 || a == 1)
+	while (j >= 0 || i >= 0 || y == 1)
 	{
-		if (b < 0)
+		if (i < 0)
 			val1 = 0;
 		else
-			val1 = *(n1 + b) - '0';
+			val1 = *(n1 + i) - '0';
 		if (j < 0)
 			val2 = 0;
 		else
 			val2 = *(n2 + j) - '0';
-		temp_tot = val1 + val2 + a;
+		temp_tot = val1 + val2 + y;
 		if (temp_tot >= 10)
-			a = 1;
+			y = 1;
 		else
-			a = 0;
+			y = 0;
 		if (digits >= (size_r - 1))
 			return (0);
 		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
 		j--;
-		b--;
+		i--;
 	}
 	if (digits == size_r)
 		return (0);
