a05dd5b43da825d53793bd594eeb4cf420e6c28b
Awalu-Levison <levisonawalu251@gmail.com>
2023-08-23 06:05:44 -0700

    The art of conversion from strings to integers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,33 +1,35 @@
 #include "main.h"
 /**
- * _atoi - Function to change string to integer
- * @s: Character variable to use
- * Return: (0 on program success)
+ * _atoi - conversion of a string to integerz
+ * @s: The string in particular
+ *
+ * Return: Generates int converted from the string
  */
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	int i, d, n, length, f, my_digit;
 
 	i = 0;
 	d = 0;
 	n = 0;
-	len = 0;
+	length = 0;
 	f = 0;
-	digit = 0;
+	my_digit = 0;
 
-	while (s[len] != '\0')
-		len++;
-	while (i < len && f == 0)
+	while (s[length] != '\0')
+		length++;
+
+	while (i < length && f == 0)
 	{
-		if (s[i] == '-');
-		++d;
+		if (s[i] == '-')
+			++d;
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			digit = s[i] - '0';
+			my_digit = s[i] - '0';
 			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
+				my_digit = -my_digit;
+			n = n * 10 + my_digit;
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
@@ -35,7 +37,9 @@ int _atoi(char *s)
 		}
 		i++;
 	}
+
 	if (f == 0)
-	return (0);
+		return (0);
+
 	return (n);
 }
