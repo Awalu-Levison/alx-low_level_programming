18e6fa9218b22879734716d03fc064dddc5682b5
Awalu-Levison <levisonawalu251@gmail.com>
2023-08-25 06:42:42 -0700

    A function that copies memory area, Prototype: char *_memcpy(char *dest, char *src, unsigned int n);

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+include "main.h"
+/**
+ * *_memcpy - A function that copies memory area
+ * @dest: Destination variable that receives bytes from src
+ * @src: The source of bytes to be used
+ * @n: Available number of bytes
+ * Return: Destination bytes
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int y = 0;
+
+	for (y < n; dest[y] != '\0'; y++)
+	{
+		while (src[y] != '\0')
+		{
+			dest[y] = src[y];
+			dest++;
+			y++;
+		}
+	}
+	return (dest);
+}
