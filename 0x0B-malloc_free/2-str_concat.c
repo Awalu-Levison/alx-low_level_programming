cf63ab216725d217911e66ee6a2f02fcf0d43ccd
Awalu-Levison <levisonawalu251@gmail.com>
2023-11-04 06:35:03 -0700

    A bfunction that concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,12 +13,12 @@ char *str_concat(char *s1, char *s2)
 	size_t a, b, c, level;
 	char *point;
 
-	if (*s1 == NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
 	}
 
-	if (*s2 == NULL)
+	if (s2 == NULL)
 	{
 		s2 = "";
 	}
