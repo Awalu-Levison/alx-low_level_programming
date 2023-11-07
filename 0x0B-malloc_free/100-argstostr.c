8c57a268af6d892a76a33251294aa2c545de0818
Awalu-Levison <levisonawalu251@gmail.com>
2023-11-06 18:09:16 -0800

    A function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,12 +12,11 @@ char *argstostr(int ac, char **av)
 	int i, n, c = 0, length = 0;
 
 	if (ac == 0 || av == NULL)
-	{
 		return (NULL);
-	}
+
 	/*Counting the arguments*/
 	for (i = 0; i < ac; i++)
-	{
+ 	{
 		for (n = 0; av[i][n]; n++)
 			length++;
 	}
