db33399dbf51e5314bf00cfb0f8229537b9b12de
Awalu-Levison <levisonawalu251@gmail.com>
2023-11-07 06:48:35 -0800

    A function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,7 +11,7 @@ char *argstostr(int ac, char **av)
 	char *pointer;
 	int i, n, k = 0, len = 0;
 
-	if (ac == 0 || av == '\0')
+	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
