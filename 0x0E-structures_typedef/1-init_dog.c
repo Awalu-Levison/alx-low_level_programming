dad8670861e91cc5073188caf8318d620d0bcfb1
Awalu-Levison <levisonawalu251@gmail.com>
2023-11-11 09:49:29 -0800

    Initixalize a variable of type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog -  Initialize a variable of type struct dog
+ * @d: Instance of dog family
+ * @name: The name of the dog
+ * @age: The age of dog
+ * @owner: The owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
