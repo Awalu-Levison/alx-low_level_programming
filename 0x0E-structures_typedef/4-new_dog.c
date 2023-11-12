7e678d64bdfdc6778726f821d35d91d2df4d8a7b
Awalu-Levison <levisonawalu251@gmail.com>
2023-11-11 23:49:54 -0800

    A function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,43 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+ * _strlen - Generates the length of a string
+ * @s: Parameter to be used
+ * Return: (0 on successful execution)
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+
+	return (0);
+}
+
+/**
+ * _strcpy - copy the string pointed to by src to dest
+ * @dest: First char to check
+ * @src: First char to check
+ * Return: 0 is success
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	dest[a] = '\0';
+	return (dest);
+}
+
+/**
  * *new_dog - A function to create new dog
  * @name: The name of dog
  * @age: Age of dog
