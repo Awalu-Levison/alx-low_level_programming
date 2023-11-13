362988338b1703381cb18dd28a4f6c69e916d5f3
Awalu-Levison <levisonawalu251@gmail.com>
2023-11-13 12:46:55 -0800

    The part of question 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,12 @@
+#include "3-calc.h"
+/**
+ * op_add - Adding two numbers
+ *
+ * @a: First number
+ * @: Second number
+ * Return: Integer value
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
