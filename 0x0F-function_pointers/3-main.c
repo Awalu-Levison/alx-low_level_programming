e87f889f00a1bdb81ffa5c088c8935b18bf63525
Awalu-Levison <levisonawalu251@gmail.com>
2023-11-15 05:47:04 -0800

    Performing simple mathematical operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,30 +9,22 @@
  */
 int main(int argc, char *argv[])
 {
-	int a, b; /*01st & 02nd number respectively*/
-	char *op;
+	int (*oprt)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	a = atoi(argv[1]);
-	op = argv[2];
-	b = atoi(argv[3]);
 
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	oprt = get_op_func(argv[2]);
+
+	if (!oprt)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*op == '/' && b <= 0) ||
-			(*op == '%' && b <= 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	printf("%d\n", get_op_func(op)(a, b));
 
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
