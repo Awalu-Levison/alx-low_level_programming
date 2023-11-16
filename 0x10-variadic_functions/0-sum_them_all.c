c371e67abde98d06cc7ce5821da9ca221bdb6742
Awalu-Levison <levisonawalu251@gmail.com>
2023-11-16 11:40:25 -0800

    Adding passed arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,15 +8,13 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list my_list;
-	unsigned int i, sum;
+	unsigned int i, sum = 0;
 
+	va_list my_list;
 	va_start(my_list, n);
 
 	if (n == 0)
 		return (0);
-
-	sum = 0;
 
 	for (i = 0, i < n; i++)
 		sum += va_arg(my_list, int);
