c1f179cce2788438a19688c99800e6817ff8193e
Awalu-Levison <levisonawalu251@gmail.com>
2023-11-16 02:38:10 -0800

    A program to print its own opcode

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Printing its own opcodes of main()
+ *
+ * @argc: argument counting
+ * @argv: actual arguments given
+ * Return: 0 Always successful
+ */
+int main(int argc, int *argv[])
+{
+	int bytes, x;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	arr = (char *)main;
+
+	for (x = 0; x < bytes; x++)
+	{
+		if (x == byes)
+		{
+			printf("%02hhx\n", arr[x]);
+			break;
+		}
+		printf("%02hhx ", arr[x]);
+	}
+	return (0);
+}
