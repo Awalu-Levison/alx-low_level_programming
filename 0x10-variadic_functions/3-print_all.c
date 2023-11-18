8c112042a6ff7275de8ee38b63160f0d97a35273
Awalu-Levison <levisonawalu251@gmail.com>
2023-11-18 15:40:28 -0800

    Printing evrything based on specifier

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+/**
+ *  print_all - Print everything according
+ *  to its type (e.g. int, char, float, string)
+ *  @format: The passed argument to the function
+ *  Return: argument passed
+ */
+void print_all(const char * const format, ...)
+{
+	int i, j;
+	char *str;
+	va_list ap;
+
+	va_start(ap, format);
+	int i = 0;
+
+	while (format != NULL & format[i] !'\0')
+	{
+		switch (format[i])
+		{
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				j = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, float));
+				j = 0;
+				break;
+			case 'c':
+				printf("%c", va_arg(ap, char));
+				j = 0;
+				break;
+			case 's':
+				str = va_arg(ap, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				break;
+			default:
+				j = 1;
+				break;
+		}
+		if (format[i + 1] != NULL && j != 0)
+			printf(", ");
+	}
+	printf("\n");
+	va_end(ap);
+}
