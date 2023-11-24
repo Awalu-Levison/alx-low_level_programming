3ff156387d917780571e6afd7268b31f5b7c6331
Awalu-Levison <levisonawalu251@gmail.com>
2023-11-23 20:05:25 -0800

    Printing the linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,16 +8,18 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t node = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
+
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		node++;
+
+		nodes++;
 		h = h->next;
 	}
-	return (node);
+	return (nodes);
 }
