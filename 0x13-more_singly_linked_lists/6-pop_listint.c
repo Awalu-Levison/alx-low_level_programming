daf78fa18b5023fd635a56d17581a48691b3a336
Awalu-Levison <levisonawalu251@gmail.com>
2023-11-27 06:54:11 -0800

    Deleting node and returning its data

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * pop_listint - Delete the node and return its data
+ * @head: The node to be deleted
+ * Return: Data of the head (n)
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *i; /*i: temporary pointer*/
+	int j;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+	n = *head->n;
+	i = *head;
+	*head = *head->next;
+	free(i);
+	return (n);
+}
