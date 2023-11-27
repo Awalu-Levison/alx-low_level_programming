efcffbc2f8b7005cc9603e9a79293634ea039e2e
Awalu-Levison <levisonawalu251@gmail.com>
2023-11-27 05:38:50 -0800

    Second part of freeing memory for a list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - Free meory of a list
+ * @head: Pointer to the first node
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *i; /*i: Temporary pointer*/
+
+	if (*head == NULL)
+		return;
+	while (*head)
+	{
+		i = *head->next;
+		free(*head);
+		*head = i;
+	}
+	head = NULL;
+}
