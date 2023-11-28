8359a03e3bff24eab85ec6c23050b952bf6ca156
Awalu-Levison <levisonawalu251@gmail.com>
2023-11-28 03:58:01 -0800

    reserving

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,23 @@
 #include "lists.h"
+listint_t *extra_node(int n);
+/**
+ * extra_node - Creating a new node
+ * @n: Data of node
+ * Return: Newly added node or NULL if fails
+ */
+listint_t *extra_node(int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	return (new_node);
+}
+
 /**
  * insert_nodeint_at_index - Add node to the linked list
  * at a given position
@@ -9,22 +28,33 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *tmp;
-	unsigned int j = 0;
-	int i = 0;
+	unsigned int i;
+	listint_t *a, *b, *new; /*a: temporary*/
 
-		new_node = malloc(sizeof(listint_t));
-		if (new_node == NULL)
-			return (NULL);
-		new_node->n = n;
-		new_node->next = NULL;
-
-		return (new_node);
-		tmp = *head;
-		tmp = tmp->next;
-		i++;
-		new_node->next = tmp->next;
-		tmp->next = new_node;
-
-	return (new_node);
+	a = *head;
+	if (head == NULL)
+		return (NULL);
+	new = extra_node(n);
+	if (new == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	if (idx == 0)
+		*head = new;
+	for (i = 0; i < idx - 1; a != NULL && idx != 0; i++)
+		a = a->next;
+	if (a == NULL)
+		return (NULL);
+	if (idx == 0)
+		new->next = a;
+	else
+	{
+		b = a->next;
+		a->next = new;
+		new->next = b;
+	}
+	return (new);
 }
