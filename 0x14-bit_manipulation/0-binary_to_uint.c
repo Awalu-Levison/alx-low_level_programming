b025db2b92a1807120da3298068560d51ed7a8c4
Awalu-Levison <levisonawalu251@gmail.com>
2023-11-29 14:57:33 -0800

    Changing numbers across limited

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * binary_to_uint - Changing from binary to unsigned integer
+ * @b: Pointer to array of characters
+ * Return: Converted number or 0 if fail
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int j = 0;
+
+	if (!j)
+		return (0);
+	for (i = 0; b[j] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
+	}
+}
