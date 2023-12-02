b11a9ebdc7af4353ae29f4e1814c7486d80c16f7
Awalu-Levison <levisonawalu251@gmail.com>
2023-12-02 02:32:08 -0800

    Creating a file in C

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,24 +1,24 @@
 #include "main.h"
 /**
- * append_text_to_file - Append text to a file
+ * create_file - A function that creates file
  * @filename: Pointer to the actual file
  * @text_content: Actual content of a file
  * Return: 1 on success and -1 on failure
  */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd; /*file descriptor*/
 	int n;
 	int f;/*rwr*/
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREATE | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
 		text_content = "";
-	for (n = 0; text_content[n], n++)
+	for (n = 0; text_content[n]; n++)
 		;
 	f = write(fd, text_content, n);
 	if (f == -1)
