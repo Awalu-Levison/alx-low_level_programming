078192ede1dd2e89ec96f40a14b4b2c652f3b4d1
Awalu-Levison <levisonawalu251@gmail.com>
2023-12-03 12:23:00 -0800

    A program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,73 +1,75 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
-#define BUFFER_SIZE 100
-void stat_checker(int stat, int fd, char *filename, char mode);
+#include <stdio.h>
+void checker(int src, int dest, char *argv[]);
 /**
- * main - Function Copies content from one file to another
- * @argc: Counting the arguments passed to the func
- * @argv: The actual arguments apssed to the func
- * Return: 1 on success, or exit on failure
+ * checker - A function that Checks for errors on file
+ * @src: The source file
+ * @dest: The destination file
+ * @argv: Arguments passed to the function
+ * Return: nothing
+ */
+void checker(int src, int dest, char *argv[])
+{
+	if (src == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (dest == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+/**
+ * main - Copying content between two files
+ * @argc: Counting the number of arguments
+ * @argv: The actual arguments passed to the function
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	/*a: src, b: dest*/
-	int a, b, n = 1024, n_write, close_a, close_b;
-	unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	char buffer[BUFFER_SIZE];
+	int src, dest, closer;
+	ssize_t n_char, n_write;
+	char buffer[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
-	a = open(argv[1], O_RDONLY);
-	stat_checker(a, -1, argv[1], 'O');
-	b = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
-	stat_checker(b, -1, argv[2], 'W');
+	src = open(argv[1], O_RDONLY);
+	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	checker(src, dest, argv);
 
-	while (n == 1024)
+	n_char = 1024;
+	while (n_char == 1024)
 	{
-		n = read(a, buffer, sizeof(buffer));
-		if (n == -1)
-			stat_checker(-1, -1, argv[1], 'O');
-		n_write = write(b, buffer, n);
-
+		n_char = read(src, buffer, 1024);
+		if (n_char == -1)
+			checker(-1, 0, argv);
+		n_write = write(dest, buffer, 1024);
 		if (n_write == -1)
-			stat_checker(-1, -1, argv[2], 'W');
+			checker(0, -1, argv);
 	}
-	close_a = close(a);
-	stat_checker(close_a, a, NULL, 'C');
-	close_b = close(b);
-	stat_checker(close_b, b, NULL, 'C');
-	return (0);
-}
-/**
- * stat_checker - Checking if file can be opened or closed
- * @stat: file descriptor - file to open
- * @fd: file descriptor - normal
- * @filename: Pointer to the actual file
- * @mode: To open and close a file
- * Return: void
- */
-void stat_checker(int stat, int fd, char *filename, char mode)
-{
-	if (mode == 'C' && stat == -1)
+
+	closer = close(src);
+	if (closer == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src);
 		exit(100);
 	}
-	else if (mode == 'O' && stat == -1)
+
+	closer = close(dest);
+	if (closer == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest);
+		exit(100);
 	}
-	else if (mode == 'W' && stat == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
-	}
+
+	return (0);
 }
