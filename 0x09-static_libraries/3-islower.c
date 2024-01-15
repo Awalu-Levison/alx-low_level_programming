9a5b4e53e73fc3f59a0f79ea0d9609408d043d51
Awalu-Levison <levisonawalu251@gmail.com>
2024-01-15 01:42:05 -0800

    Creating static library in C

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - Checking if character is lower case
+ * @c: Parameter to check
+ * Return: 1 lowercase or 0 for others
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
