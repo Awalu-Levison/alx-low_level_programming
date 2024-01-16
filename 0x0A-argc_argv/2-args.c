11a7f94ddf738f5b66215b5622050cb8798a382a
Awalu-Levison <levisonawalu251@gmail.com>
2024-01-15 18:29:12 -0800

    Printing args separated by new line

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Entry pointy of program
+ * @argc: Argument count
+ * @argv: Arguments passed
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv);
+	return (0);
+}
