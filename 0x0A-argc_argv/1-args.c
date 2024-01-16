9db863eed988276c231ca4605c9c25089eb5a877
Awalu-Levison <levisonawalu251@gmail.com>
2024-01-15 18:04:38 -0800

    A function that calculates number of arguments passed via commandline

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point of program
+ * @argc: Argument count
+ * @argv: Arguments passed to the function on commansline
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
