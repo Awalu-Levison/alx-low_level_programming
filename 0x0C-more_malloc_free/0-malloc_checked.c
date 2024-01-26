dd479fd293efceab4a4c01881e3e2905f833d95b
Awalu-Levison <levisonawalu251@gmail.com>
2024-01-26 13:10:02 -0800

    Memory allocation

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *malloc_checked - A function that allocates memory
+ * @b: Parameter to be allocated memory
+ * Return: Pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *ptr;
+
+	ptr = malloc(sizeof(b));
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	else
+		return (ptr);
+}
