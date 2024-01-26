315caf367b9cd047b390ce789e7176a4d51d233c
Awalu-Levison <levisonawalu251@gmail.com>
2024-01-26 15:30:47 -0800

    concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,27 +20,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 		len1++;
 	for (i = 0; s2[i] != '\0'; i++)
-	{
 		len2++;
-	}
+
 	ptr = malloc(sizeof(char) * (len1 + n) + 1);
 	if (ptr == NULL)
 		return (NULL);
+
 	if (n >= len2)
 	{
 		for (i = 0; s1[len1] != '\0'; i++)
 			ptr[i] = s1[i];
 		for (i = 0; s2[i] != '\0'; i++)
-			ptr[len1 + 1] = s2[i];
-		ptr[len1 + 1] = '\0';
+			ptr[len1 + i] = s2[i];
+		ptr[len1 + i] = '\0';
 	}
 	else
 	{
 		for (i = 0; s1[i] != '\0'; i++)
 			ptr[i] = s1[i];
 		for (i = 0; i < n; i++)
-			ptr[len1 + 1] = s2[i];
-		ptr[len1 + 1] = '\0';
+			ptr[len1 + i] = s2[i];
+		ptr[len1 + i] = '\0';
 	}
 	return (ptr);
+	free(ptr);
 }
