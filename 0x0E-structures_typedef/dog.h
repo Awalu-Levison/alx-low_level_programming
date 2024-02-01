4691234a6f46e8b51a795635589fef9773b48df6
Awalu-Levison <levisonawalu251@gmail.com>
2024-02-01 15:10:08 -0800

    Updating dog header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,23 @@
  */
 struct dog
 {
-	struct dog dog_t
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * struct dog - Structure of new dog
+ * dog_t - New dog
+ * @name: The name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ */
+struct dog dog_t
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
