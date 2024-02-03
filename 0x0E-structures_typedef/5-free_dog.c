20ac63dfb0123dbfe58f47bb109f467c522a12a5
Awalu-Levison <levisonawalu251@gmail.com>
2024-02-03 01:46:42 -0800

    A function that frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - A function that frees the structure of a dog
+ * @d: Pointer to the struct of dog
+ */
+void free_dog(dog_t *d)
+{
+	while (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
