54ee3c8ea80ab02452122d1e62a335e8d5f44136
Awalu-Levison <levisonawalu251@gmail.com>
2024-02-18 20:47:22 -0800

    Prototype of task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * binary_to_uint - Converting binary to unsigned int
+ * @b: A characterparameter to be used
+ * Return: The converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+}
