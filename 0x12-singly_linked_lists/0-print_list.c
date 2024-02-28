1e04cbdaecfd82b8659ed16ebae28467975e1828
Awalu-Levison <levisonawalu251@gmail.com>
2024-02-27 21:46:20 -0800

    Printing the linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -19,9 +19,7 @@ size_t print_list(const list_t *h)
 			printf("%u %s\n", h->len, "(nil)");
 		}
 		else
-		{
 			printf("%u %s\n", h->len, h->str);
-		}
 		h = h->next;
 	}
 	printf("%u %s\n", h->len, "(nil)");
