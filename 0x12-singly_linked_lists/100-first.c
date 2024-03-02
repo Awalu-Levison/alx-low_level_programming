27fef899c9092864777b6430c208691ef53deec9
Awalu-Levison <levisonawalu251@gmail.com>
2024-03-02 08:10:49 -0800

    Executing certain code before the main function executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+void runFirstbyAWALU(void) __attribute__((constructor));
+/**
+ * runFirstbyAWALU - Running certain code before
+ * main function gets executed
+ * Return: void
+ */
+void runFirstbyAWALU(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
