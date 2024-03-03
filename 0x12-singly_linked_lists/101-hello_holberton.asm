714a07349b2c50dea03fe60e943e28dfb477c7a7
Awalu-Levison <levisonawalu251@gmail.com>
2024-03-03 06:45:24 -0800

    Writing assembly language program that prints hello, holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,22 @@
-global	main
 	extern	printf
+
+	        section .data			; Variable declarations
+msg:		db "Hello, Holberton", 0
+fmt:	    	db "%s", 10, 0			; Formating the stdout
+author:		db "Awalu Levison"		; Origin of work
+
+	        section .text			; Content of write function
+
+		global main			; Program entry point
 main:
-	mov edi, format
-	xor eax, eax
-	call printf
-	mov	eax, 0
-	ret
-format: db 'Hello, Holberton\n', 0
+		push	rbp			; set up stack frame memory
+
+		mov	rdi,fmt
+		mov	rsi,msg			; Define msg var
+		mov	rax,0
+	        call    printf			; Calling the function
+
+		pop	rbp			; Restore the stack memory
+
+		mov	rax,0			; Indicates succeful execution
+		ret				; return
