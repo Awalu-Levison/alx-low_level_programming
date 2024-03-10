83e3881bb24dcf8921568ee01a8b18333cb1d81b
Awalu-Levison <levisonawalu251@gmail.com>
2024-03-10 01:28:11 -0800

    Updating header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,20 +18,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-
-/**
- * struct mylist - Structure for my own list
- * @p: Void pointer
- * @next: Pointer to the next
- *
- * Description: Singly linked list
- */
-typedef struct mylist
-{
-	void *p;
-	struct mylist *next;
-} list_s;
-
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
