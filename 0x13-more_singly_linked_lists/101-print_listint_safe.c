f8e3bb1dcae1b9208b74472994a37bb75b34a6c2
Awalu-Levison <levisonawalu251@gmail.com>
2024-03-10 01:18:00 -0800

    Printing the linked list without memory leak

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,67 +1,64 @@
 #include "lists.h"
 #include <stdio.h>
 
-size_t looped_listint_len(const listint_t *head);
+size_t list_looping(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
 
 /**
- * looped_listint_len - Counts the number of unique nodes
- * in a looped listint_t linked list.
- * @head: A pointer to the head of the listint_t to check.
- *
- * Return: If the list is not looped - 0.
- * Otherwise - the number of unique nodes in the list.
+ * list_looping - Counting the number of nodes in a list.
+ * @head: Pointer to trhe first node
+ * Return: Number of nodes in the list.
  */
-size_t looped_listint_len(const listint_t *head)
+size_t list_looping(const listint_t *head)
 {
-	const listint_t *tortoise, *hare;
-	size_t nodes = 1;
+	const listint_t *ptr1, *ptr2;
+	size_t i = 1; /*counts nodes*/
 
 	if (head == NULL || head->next == NULL)
 		return (0);
 
-	tortoise = head->next;
-	hare = (head->next)->next;
+	ptr1 = head->next;
+	ptr2 = (head->next)->next;
 
-	while (hare)
+	while (ptr2)
 	{
-		if (tortoise == hare)
+		if (ptr1 == ptr2)
 		{
-			tortoise = head;
-			while (tortoise != hare)
+			ptr1 = head;
+			while (ptr1 != ptr2)
 			{
-				nodes++;
-				tortoise = tortoise->next;
-				hare = hare->next;
+				i++;
+				ptr1 = ptr1->next;
+				ptr2 = ptr2->next;
 			}
-			tortoise = tortoise->next;
-			while (tortoise != hare)
+			ptr1 = ptr1->next;
+			while (ptr1 != ptr2)
 			{
-				nodes++;
-				tortoise = tortoise->next;
+				i++;
+				ptr1 = ptr1->next;
 			}
-			return (nodes);
+			return (i);
 		}
-		tortoise = tortoise->next;
-		hare = (hare->next)->next;
+		ptr1 = ptr1->next;
+		ptr2 = (ptr2->next)->next;
 	}
 	return (0);
 }
 
 /**
- * print_listint_safe - Prints a listint_t list safely.
- * @head: A pointer to the head of the listint_t list.
+ * print_listint_safe - Printing list without memory leak.
+ * @head: Pointer to the first node
  * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes, index = 0;
+	size_t i, idx = 0;
 
-	nodes = looped_listint_len(head);
+	i = list_looping(head);
 
-	if (nodes == 0)
+	if (i == 0)
 	{
-		for (; head != NULL; nodes++)
+		for (; head != NULL; i++)
 		{
 			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
@@ -69,12 +66,12 @@ size_t print_listint_safe(const listint_t *head)
 	}
 	else
 	{
-		for (index = 0; index < nodes; index++)
+		for (idx = 0; idx < i; idx++)
 		{
 			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
 		}
 		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
-	return (nodes);
+	return (i);
 }
