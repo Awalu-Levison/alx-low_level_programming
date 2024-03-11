97eb4815f8f71e774db86ce52d97bcf497d39418
Awalu-Levison <levisonawalu251@gmail.com>
2024-03-11 02:40:29 -0700

    Reading text from a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,6 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
@@ -23,6 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
+
 	file_read = read(fd, buffer, letters);
 	if (file_read == -1)
 	{
