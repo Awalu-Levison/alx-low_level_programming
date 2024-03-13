8b1b42de1e0222a4eeea734605c5a125f9ca874e
Awalu-Levison <levisonawalu251@gmail.com>
2024-03-13 07:12:40 -0700

    A function that copies content from one file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,12 @@
 #include "main.h"
+void checker(int fd, int state, char *filename, char mode);
+
 /**
- * checker - Checking if file can be opened or closed
- * @fd: File descriptor
+ * checker - Checking the file condition
+ * @fd: The file descriptor
+ * @state: State of the file fail or success
  * @filename: Pointer to the actual name
- * @mode: The file access mode 
- * @state: The state of the file
+ * @mode: The mode of the file access
  */
 void checker(int fd, int state, char *filename, char mode)
 {
@@ -13,12 +15,12 @@ void checker(int fd, int state, char *filename, char mode)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
-	else if (mode == 'O' && state == -1)
+	else if (mode == 'O' && state == 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
 		exit(98);
 	}
-	else if (mode == 'w' && state == -1)
+	else if (mode == 'W' && state == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
 		exit(99);
@@ -26,24 +28,37 @@ void checker(int fd, int state, char *filename, char mode)
 }
 
 /**
- * main - A program that copies the content of a file to another file
- * @argc: Number of arguments
- * @argv: The actua; args
- * Return: 0
+ * main - A function that copies contents from one file to another file
+ * @argc: Argumentys counting
+ * @argv: Actual arguments
+ * Return: 0 on sucess
  */
-#define BUFFER 1024;
-
 int main(int argc, char *argv[])
 {
-	int src, dest, dest_close, src_close, file_write, n = 1024;
-
+	int src, dest, file_write;
 	unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-
 	char buffer[1024];
+	ssize_t n = 0;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
+	src = open(argv[1], O_RDONLY);
+	checker(src, src, argv[1], 'O');
+	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	checker(dest, dest, argv[2], 'W');
+
+	while ((n = read(src, buffer, sizeof(buffer))) > 0)
+	{
+		if (n == -1)
+			checker(-1, -1, argv[1], 'O');
+		file_write = write(dest, buffer, n);
+		if (file_write == -1)
+			checker(-1, -1, argv[2], 'W');
+	}
+	close(src);
+	close(dest);
+	return (0);
 }
