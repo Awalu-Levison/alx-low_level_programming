fe20ced91746e39b5f2f5349a805b63ba1f9f83a
Awalu-Levison <levisonawalu251@gmail.com>
2024-04-21 15:20:52 -0700

    Return number of elements only

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,14 +7,20 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t i = 0;
+	int i = 0;
 
-	const dlistint_t *temp = h;
+	/*const dlistint_t *temp = h;*/
 
-	while (temp != NULL)
+	if (h == NULL)
+		return (i);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
 	{
-		temp = temp->next;
 		i++;
+		h = h->next;
 	}
 	return (i);
 }
