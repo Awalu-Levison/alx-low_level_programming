50c9955cce45be7b35050ec49b738da95d9eadb5
Awalu-Levison <levisonawalu251@gmail.com>
2024-04-23 14:48:00 -0700

    Add node at the end of the lists.

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -17,7 +17,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->prev = NULL;
+	new_node->next = NULL;
 	new_node->n = n;
 
 	/*Handle if list is empty*/
