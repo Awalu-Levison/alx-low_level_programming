d8e882d2386b36cf688789deba23b6c97a054610
Awalu-Levison <levisonawalu251@gmail.com>
2024-04-28 02:18:25 -0700

    Summing up of all data in a doubly linked list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,16 +1,14 @@
 #include "lists.h"
+
 /**
- * sum_dlistint - A function that return sum of all data
- * in a doubly linked list
- * @head: Reference to the first node
- * Return: Sum of all data
+ * sum_dlistint - Sum up of all data in a doubly linked list
+ * @head: Reference to the first node of the linked list
+ *
+ * Return: sum of all data, or 0 if the list is empty
  */
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
-
-	if (head == NULL)
-		return (0);
 
 	while (head != NULL)
 	{
