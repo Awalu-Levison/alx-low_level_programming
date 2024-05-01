8040e1c8d81a8a145f7dcf229f49b760eef73a35
Awalu-Levison <levisonawalu251@gmail.com>
2024-04-30 22:04:46 -0700

    Delete a node at given index in doubly linked list

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -27,6 +27,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 				*head = ptr1->next;
 				if (*head != NULL)
 					(*head)->prev = NULL;
+				free(ptr1);
+				return (1);
 			}
 			else
 			{
@@ -35,8 +37,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 				if (ptr1->next != NULL)
 					ptr1->next->prev = ptr2;
 			}
-			free(ptr1);
-			return (1);
+			/*free(ptr1);*/
+			/*return (1);*/
 		}
 		ptr2 = ptr1;
 		ptr1 = ptr1->next;
