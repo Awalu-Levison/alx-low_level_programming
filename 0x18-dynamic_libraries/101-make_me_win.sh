a2069c229fc31a5e8eb1214311a4859369f8ba76
Awalu-Levison <levisonawalu251@gmail.com>
2024-05-05 14:41:32 -0700

    A script that run a code for Jackpot hack

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Awalu-Levison/alx-low_level_programming/master/0x18-dynamic_libraries/libdymyhack.so
-export LD_PRELOAD="$PWD/../libdymyhack.so"
+wget -P .. https://raw.githubusercontent.com/Awalu-Levison/alx-low_level_programming/master/0x18-dynamic_libraries/libdynamicmyhack.so
+export LD_PRELOAD="$PWD/../libdynamicmyhack.so"
