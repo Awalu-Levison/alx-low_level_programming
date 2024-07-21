c5e1909ad3b69d1113bda525cf00c9fa738416f5
Awalu-Levison <levisonawalu251@gmail.com>
2024-07-20 17:30:47 -0700

    Update header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -49,5 +49,6 @@ int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 int binary_search2(int *array, size_t a, size_t b, int value);
+int advanced_binary_search_rec(int *array, size_t a, size_t b, int value);
 
 #endif
